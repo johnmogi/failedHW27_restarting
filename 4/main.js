f3c69392a4520c915047d701b360be0251b1fd33
johnmogi <anguru@gmail.com>
2019-11-15 05:39:47 -0500

    4

---
+++ 4/main.js
@@ -0,0 +1,73 @@
+/// <reference path="jquery-3.4.1.js" />
+
+$(() => {
+    // const colorArr = ["red", "green", "blue", "yellow", "magenta", "cyan", "black", "white", "gray" ]
+ 
+    const oneNum = parseInt(prompt("give me the first number"));
+    const twoNum = parseInt(prompt("give me the second number"));
+    
+    let numArray = []
+numArray.push(oneNum)
+numArray.push(twoNum)
+console.log(numArray)
+
+    function getMiddlePrimaryNumbers(min, max) {
+        numArray = [1,2,3,10]
+        getAverage(numArray);  
+    
+    }
+
+    function getPrice2(nums) {
+      numArray =[]
+        getAverage(numArray);  
+    }
+ 
+    function getPrice3(nums) {
+        numArray = null
+          getAverage(numArray);  
+      }
+      function getPrice4(nums) {
+        numArray = "";
+          getAverage(numArray);  
+      }
+    function getAverage(average){
+       
+var total=0;
+for(var i in numArray) { total += numArray[i]; }
+
+
+try {
+    if(numArray.length>0 || numArray != "" ){
+
+        const avgNum = total / numArray.length
+        alert("Success : the average num is: "+ avgNum)
+return avgNum
+    }
+    else{
+    
+            throw new Error();
+    
+    }
+}
+catch (err) {
+    alert("Error :  please try again, the numbers array is : " +  numArray)
+    // console.log(numArray)
+}
+}
+
+
+
+    $("#one").click(() => {
+    getPrice();
+});
+$("#two").click(() => {
+    getPrice2();
+});
+$("#three").click(() => {
+    getPrice3();
+});
+$("#four").click(() => {
+    getPrice4();
+});
+    }); //RF
+    
